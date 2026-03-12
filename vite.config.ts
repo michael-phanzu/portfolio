fc7150e1c0c268a87e680c2c12ad255ff7dcb249
michael-phanzu <michaelphanzu11@gmail.com>
2026-03-12 01:24:39 +0100

    Fix: Correction des imports button et ajout de utils.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
+      "@lib": path.resolve(__dirname, "./src/lib/"),
+      "@components": path.resolve(__dirname, "./src/components/"),
     },
   },
 });
